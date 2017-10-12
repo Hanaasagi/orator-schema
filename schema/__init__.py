fc2564812053d3b8dbc17c9d1e57752817fac88c
cuikunqiang <kunqiang.cui@shanbay.com>
2017-10-12 14:08:49 +0800

    update

--- schema/__init__.py
+++ schema/__init__.py
@@ -6,21 +6,53 @@ code = """
 from orator.migrations import Migration
 
 
-class CreateTableUsers(Migration):
+class InitDb(Migration):
 
     def up(self):
-        with self.schema.create('users') as table:
+        with self.schema.create('ads') as table:
             table.increments('id')
-            table.timestamps()
-            table.foreign('ad_place_id').reference('id').on('ad_places').on_delete('cascade')
-
-        with self.schema.create('finends') as table:
-            table.increments('id')
-            table.integer('user_id')
+            table.string('name')
+            table.string('url')
+            table.datetime('started_at')
+            table.datetime('ended_at')
+            table.string('image').unique()
+            table.json('product_ids')
+            table.small_integer('status')
+            table.integer('ad_place_id')
             table.timestamps()
 
     def down(self):
-        self.schema.drop('users')
+        self.schema.drop('ads')
+"""
+
+code_2 = """
+from orator.migrations import Migration
+
+
+class InitDb(Migration):
+
+    def up(self):
+        with self.schema.create('ads') as table:
+            table.string('name').default('23333').change()
+            table.string('biubiubiu')
+            table.drop_column('url')
+            table.small_integer('status').unsigned().change()
+            table.integer('ad_place_id').nullable().change()
+            table.index(['name', 'biubiubiu'])
+"""
+
+
+code_3 = """
+from orator.migrations import Migration
+
+
+class InitDb(Migration):
+
+    def up(self):
+        with self.schema.create('ads') as table:
+            table.integer('ad_place_id').change()
+            table.string('biubiubiu', 50).change()
+            # table.drop_index('ads_name_biubiubiu_index')
 """
 
 COMMAND = {
@@ -58,10 +90,6 @@ OPTIONAL = {
     'unsigned': '',
 }
 
-ast_code = ast.parse(code)
-
-gen = ast.walk(ast_code)
-
 collection = []
 
 default_dict = lambda: defaultdict(default_dict)
@@ -80,22 +108,31 @@ tmp_test = []
 
 CONSTRAINT = {
     'nullable': lambda field: field.set_null(),
-    'unique': lambda field: field.set_unique(),
     'reference': lambda field, *args: field.set_reference(*args),
     'on': lambda field, *args: field.set_reference(*args),
+    'unsigned': lambda field: field.set_unsigned(),
     'on_delete': lambda field, *args: field.set_on_delete(),
+    'default': lambda field, *args: field.set_default(*args),
 }
 
 
 class Field:
 
-    def __init__(self, name, type_):
+    def __init__(self, name, type_, *args, **kwargs):
         # name is a redundant field
         self._name = name
         self._type_ = type_
-        self._ref_table = None
-        self._ref_col = None
-        self.others = None
+        if args or kwargs:
+            self.handle_type_args(*args, **kwargs)
+
+    def handle_type_args(self, *args, **kwargs):
+        raise NotImplementedError()
+
+    def reset(self):
+        self._type_ = None
+        self._unique = False
+        self._nullable = False
+        self._default = None
 
     def set_name(self, new_name):
         self._name = new_name
@@ -104,13 +141,7 @@ class Field:
         self._type_ = new_type
 
     def set_null(self):
-        self.allow_null = True
-
-    def set_unique(self):
-        self._unique = True
-
-    def remove_unique(self):
-        self._unique = False
+        self._nullable = True
 
     def set_default(self, value):
         self._default = value
@@ -123,10 +154,45 @@ class Field:
         return '<{}, __dict__={}>'.format(
             obj_info.strip('<>'), self.__dict__)
 
-    def __dump__(self):
+    def _get_meta(self):
         meta = defaultdict(lambda: '')
         meta.update(self.__dict__)
-        return 'table.{_type_}({_name}).{_unique}()'.format_map(meta)
+        return meta
+
+    def __dump__(self):
+        meta = self._get_meta()
+        tmpl = ["table.{_type_}('{_name}')".format_map(meta)]
+        meta.pop('_type_')
+        meta.pop('_name')
+        for k in filter(lambda s: s.startswith('_') and not s.startswith('__'),
+                        meta.keys()):
+            v = meta[k]
+            # TODO default
+            if v:
+                if k == '_default':
+                    tmpl.append(".{}('{}')".format(k.lstrip('_'), v))
+                else:
+                    tmpl.append('.{}()'.format(k.lstrip('_')))
+        return ''.join(tmpl)
+        # return "table.{_type_}('{_name}').{_unique}()".format_map(meta)
+
+
+class IntegerField(Field):
+
+    def set_unsigned(self):
+        self._unsigned = True
+
+    def reset(self):
+        self._unsigned = False
+        super().reset()
+
+
+class SmallIntegerField(IntegerField):
+    pass
+
+
+class BigIntegerField(IntegerField):
+    pass
 
 
 class StringField(Field):
@@ -134,8 +200,54 @@ class StringField(Field):
     def set_length(self, length):
         self._length = length
 
+    def handle_type_args(self, *args, **kwargs):
+        assert len(args) > 0 or 'length' in kwargs
+        length = args[0] if len(args) > 0 else kwargs.pop('length')
+        self.set_length(length)
+
+    def __dump__(self):
+        meta = self._get_meta()
+        tmpl = ["table.{_type_}('{_name}')".format_map(meta)]
+        if hasattr(self, '_length'):
+            tmpl[0] = tmpl[0].rstrip(')')
+            tmpl.append(", {_length})".format_map(meta))
+            meta.pop('_length')
+        meta.pop('_type_')
+        meta.pop('_name')
+        for k in filter(lambda s: s.startswith('_') and not s.startswith('__'),
+                        meta.keys()):
+            v = meta[k]
+            if v:
+                tmpl.append('.{}()'.format(k.lstrip('_')))
+        return ''.join(tmpl)
+
+
+class TimestampsField(Field):
+
+    def __dump__(self):
+        meta = self._get_meta()
+        tmpl = ["table.{_type_}()".format_map(meta)]
+        meta.pop('_type_')
+        meta.pop('_name')
+        for k in filter(lambda s: s.startswith('_') and not s.startswith('__'),
+                        meta.keys()):
+            v = meta[k]
+            if v:
+                tmpl.append('.{}()'.format(k.lstrip('_')))
+        return ''.join(tmpl)
+
+
+class TimestampField(Field):
+    pass
+
 
 class ForeignField(Field):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._ref_table = None
+        self._ref_col = None
+        self.others = None
 
     def set_reference(self, ref_col):
         self._ref_col = ref_col
@@ -149,15 +261,19 @@ class ForeignField(Field):
 
 class FieldFactory:
 
+    mapping = {
+        'string': StringField,
+        'foreign': ForeignField,
+        'small_integer': SmallIntegerField,
+        'timestamp': TimestampField,
+        'timestamps': TimestampsField,
+    }
+
+    default = Field
+
     @classmethod
     def create(cls, name, type_, *args, **kwargs):
-        kls = Field
-        if type_ == 'string':
-            kls = StringField
-        elif type_ == 'foreign':
-            kls = ForeignField
-        else:
-            kls = Field
+        kls = cls.mapping.get(type_, cls.default)
         return kls(name, type_, *args, **kwargs)
 
 
@@ -180,7 +296,6 @@ def get_value(node):
     elif isinstance(node, ast.List):
         return [get_value(x) for x in node.elts]
     else:
-        tmp_test.append(node)
         raise TypeError('Unknown AST Type {}'.format(node))
 
 
@@ -191,9 +306,17 @@ def get_type(node):
     return node.func.attr
 
 
-def handle_field_constraint(field, nodes):
+INDEX_TYPE = ('primary', 'index', 'unique')
+
+
+def handle_field_constraint(table_name, field, nodes):
     while len(nodes) > 0:
         node = nodes.pop(0)
+        type_ = get_type(node)
+        if type_ in ('primary', 'index', 'unique'):
+            node.args = [ast.Str(field._name)]
+            handle_indexs(table_name, node)
+            return
         CONSTRAINT[get_type(node)](field, *[get_value(x) for x in node.args])
 
 
@@ -208,7 +331,7 @@ def to_list(arg):
         return [arg]
 
 
-def handle_index(table_name, node):
+def handle_indexs(table_name, node):
     args = node.args
     assert len(args) == 1
     cols = get_value(args.pop(0))
@@ -221,7 +344,7 @@ def handle_index(table_name, node):
 
 
 def do_drop_index(table_name, type_, cols):
-    print(schema[table_name]['_ORM_META'])
+    # print(schema[table_name]['_ORM_META'])
     schema[table_name]['_ORM_META'].pop(cols)
 
 
@@ -230,7 +353,7 @@ def do_add_index(table_name, type_, cols):
     schema[table_name]['_ORM_META'][index_name] = type_
 
 
-def do_cahnge_col(table_name, node):
+def do_change_col(table_name, node):
     """handle like following:
        table.string('name', 50).nullable().change()
     """
@@ -248,8 +371,11 @@ def do_cahnge_col(table_name, node):
     assert field_name in schema[table_name]
     field_obj = schema[table_name][field_name]
     assert isinstance(field_obj, Field)
+    field_obj.reset()
     field_obj.set_type(field_type)
-    handle_field_constraint(field_obj, path)
+    if len(args) > 0:
+        field_obj.handle_type_args(*map(get_value, args))
+    handle_field_constraint(table_name, field_obj, path)
 
 
 def do_drop_col(table_name, cols):
@@ -272,13 +398,13 @@ def do_add_col(table_name, obj):
 
     field = path.pop(0)
     field_type = get_type(field)
-    # Really here?
+    # Really here? ugly
     field_name, *args = field.args or [ast.Str('timestamps')]
     field_name = get_value(field_name)
     assert field_name not in schema[table_name]
     field_obj = schema[table_name][field_name] = FieldFactory.create(
-        field_name, field_type)
-    handle_field_constraint(field_obj, path)
+        field_name, field_type, *args)
+    handle_field_constraint(table_name, field_obj, path)
 
 
 class Builder(ast.NodeVisitor):
@@ -299,12 +425,12 @@ class Builder(ast.NodeVisitor):
                 is_index = False
 
             if is_index:
-                handle_index(table_name, obj)
+                handle_indexs(table_name, obj)
                 continue
 
             # do some action
             if obj.func.attr == ACTION.change:
-                do_cahnge_col(table_name, obj.func.value)
+                do_change_col(table_name, obj.func.value)
             elif obj.func.attr == ACTION.rename_column:
                 assert len(obj.args) == 2
                 do_rename_col(table_name, *obj.args)
@@ -325,17 +451,6 @@ class Builder(ast.NodeVisitor):
                 self._generate_table(table_name, ast_with.body.copy())
 
 
-TABLE_TMPL = """\
-CREATE TABLE {} (
-    {}
-)
-"""
-
-TABLE_CLASS_TMPL = """\
-class {}:
-"""
-
-
 def flatten(struct):
     result = []
 
@@ -352,12 +467,25 @@ def flatten(struct):
     return result
 
 
-if __name__ == '__main__':
-    import json
-    Builder().visit(ast_code)
-    table = {}
+def dump():
     for table_name, table_field in schema.items():
-        print(table_name)
-        for field_name, keyword in table_field.items():
-            print(keyword.__dump__())
+        print("with self.schema.create('{}') as table:".format(table_name))
+        for field_name, keyword in sorted(table_field.items()):
+            if field_name == '_ORM_META':
+                for k, v in keyword.items():
+                    temp = list(map(lambda x: "'" + x + "'",
+                                    k.split('_')[1:-1]))
+                    k = ','.join(temp)
+                    if len(temp) > 1:
+                        k = '[{}]'.format(k)
+                    print('    ', 'table.{}({})'.format(v, k))
+            else:
+                print('    ', keyword.__dump__())
         print('\n')
+
+
+if __name__ == '__main__':
+    for code in (code, code_2, code_3):
+        ast_code = ast.parse(code)
+        Builder().visit(ast_code)
+    dump()
